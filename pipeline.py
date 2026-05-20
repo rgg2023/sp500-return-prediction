bcbd0ec3c3e4c772a7fcb18e66d0db21cedfe0d5
rgg2023 <daphne.kim922@gmail.com>
2026-05-20 22:39:21 +0900

    pipeline.py 설정 완료

---
+++ pipeline.py
@@ -0,0 +1,9 @@
+def run_data_science_pipeline(data_path, scaling_method, encoding_method, model_type, hyperparams):
+    # 1. Preprocessing (전처리 담당자 코드가 들어갈 자리)
+    # X_train, X_test, y_train, y_test = preprocess_data(data_path, scaling_method, encoding_method)
+
+    # 2. Model Training & Testing (모델링 담당자 코드가 들어갈 자리)
+    # results = train_and_evaluate(X_train, X_test, y_train, y_test, model_type, hyperparams)
+
+    # 3. Return top combinations
+    return results
